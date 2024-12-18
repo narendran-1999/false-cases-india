932bedd49e7852a07c33f1e6869301390d86f266
narendran-1999 <narendranrita@gmail.com>
2024-12-18 21:34:51 +0530

    metro cities list component

--- false-cases-react/src/components/CitiesList/CitiesList.jsx
+++ false-cases-react/src/components/CitiesList/CitiesList.jsx
@@ -1,15 +1,38 @@
 import React from 'react'
 import './CitiesList.css'
 
+const cities = {
+  Bihar: ["Patna"],
+  "Delhi (NCR)": ["Delhi City"],
+  Gujarat: ["Ahmedabad", "Surat"],
+  Karnataka: ["Bengaluru"],
+  Kerala: ["Kochi", "Kozhikode"],
+  "Madhya Pradesh": ["Indore"],
+  Maharashtra: ["Mumbai", "Nagpur", "Pune"],
+  Rajasthan: ["Jaipur"],
+  "Tamil Nadu": ["Chennai", "Coimbatore"],
+  Telangana: ["Hyderabad"],
+  "Uttar Pradesh": ["Ghaziabad", "Kanpur", "Lucknow"],
+  "West Bengal": ["Kolkata"]
+}
+
 const CitiesList = () => {
   return (
     <div className='metro-cities-list'>
         <div className="cities-head">
           LISTED CITIES (19)
-        </div><br/>
+        </div>
 
         <div className="cities-body">
-
+          {
+            Object.entries(cities).map(([state, cities]) => (
+              <div key={state} className='state-box'>
+                {cities.map((city) => (
+                  <div key={city} className='city-item'>{city}</div>
+                ))}
+              </div>
+            ))
+          }
         </div>
     </div>
   )
