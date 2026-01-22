e57689a5b95d4df992d584464f7f5550844394a9
narendran-1999 <narendranrita@gmail.com>
2026-01-22 13:43:40 +0530

    Implemented data processing in chart rendering components; Updated percent calculation in index.astro to use slice & reduce

--- false-cases-astro/src/components/DataVis/RenderPie.svelte
+++ false-cases-astro/src/components/DataVis/RenderPie.svelte
@@ -2,15 +2,29 @@
     import { type ProperTitleKey } from "../../data/constants-and-types";
     import { getLatestCrimeData } from "../../data/get-data";
 
+    // Handle Data Fetching & Calculation based on Props
     const { crime, tabIndiaMetro }: {
         crime: ProperTitleKey,
         tabIndiaMetro: 0 | 1
     } = $props();
 
-    const {
-        data: latestCrimeData,
-        title: crimeTitle
-    } = getLatestCrimeData(crime, (tabIndiaMetro === 1) ? true : false);
+    $effect(() => {
+        const isMetro = (tabIndiaMetro === 1) ? true : false;
+
+        const {
+            data,
+            title
+        } = getLatestCrimeData(crime, isMetro);
+
+        // Quashed Cases excluded
+        const fakeNum = data.slice(1, 4).reduce((a, b) => a + b, 0);
+        const maliciousFalseNum = fakeNum - data[3];
+        const convictedNum = data[6];
+
+        const maliciousFalsePercent = (maliciousFalseNum / (fakeNum + convictedNum)) * 100;
+        const fakePercent = (fakeNum / (fakeNum + convictedNum)) * 100;
+        const convictedPercent = 100 - (fakePercent);
+    });
 </script>
 
 <div class="">
--- false-cases-astro/src/components/DataVis/RenderTrend.svelte
+++ false-cases-astro/src/components/DataVis/RenderTrend.svelte
@@ -2,16 +2,70 @@
     import { type ProperTitleKey } from "../../data/constants-and-types";
     import { getCrimeData } from "../../data/get-data";
 
+    // Define Trend Data Model
+    type TrendDataModel = {
+        year: number,
+        maliciousFalseNum: number,
+        fakeNum: number
+    } | {
+        year: number,
+        maliciousFalsePercent: number,
+        fakePercent: number
+    };
+
+    // Handle Data Fetching & Calculation based on Props
     const { crime, tabIndiaMetro, trendType }: {
         crime: ProperTitleKey,
         tabIndiaMetro: 0 | 1,
         trendType: "cases" | "prevalence"
     } = $props();
 
-    const {
-        data: crimeTrendData,
-        title: crimeTitle
-    } = getCrimeData(crime, (tabIndiaMetro === 1) ? true : false);
+    let trendData: TrendDataModel[] = [];
+
+    $effect(() => {
+        const isMetro = (tabIndiaMetro === 1) ? true : false;
+
+        const {
+            data,
+            title
+        } = getCrimeData(crime, isMetro);
+
+        trendData = [];
+
+        if (trendType === "cases") {
+            // Process data for Cases Trend
+
+            // Quashed Cases included
+            data.map((yearData) => {
+                const fakeNum = yearData.slice(1, 6).reduce((a, b) => a + b, 0);
+                const maliciousFalseNum = fakeNum - yearData[3];
+
+                trendData.push({
+                    year: yearData[0],
+                    maliciousFalseNum,
+                    fakeNum
+                });
+            });
+        } else {
+            // Process data for Prevalence Trend
+
+            // Quashed Cases excluded
+            data.map((yearData) => {
+                const fakeNum = yearData.slice(1, 4).reduce((a, b) => a + b, 0);
+                const maliciousFalseNum = fakeNum - yearData[3];
+                const convictedNum = yearData[6];
+
+                const maliciousFalsePercent = (maliciousFalseNum / (fakeNum + convictedNum)) * 100;
+                const fakePercent = (fakeNum / (fakeNum + convictedNum)) * 100;
+
+                trendData.push({
+                    year: yearData[0],
+                    maliciousFalsePercent,
+                    fakePercent
+                });
+            });
+        }
+    });
 </script>
 
 <div class="">
