ea0aa558a81ea7f8294003bfa056279b9a66152a
narendran-1999 <narendranrita@gmail.com>
2026-03-07 00:35:53 +0530

    Added active button stylings

--- false-cases-astro/src/components/Header/menuItems.ts
+++ false-cases-astro/src/components/Header/menuItems.ts
@@ -5,8 +5,20 @@ type menuItem = {
 
 // Define the menu items for header
 export const menuItems: menuItem[] = [
-    { title: 'Home', href: '' },
-    { title: 'FAQs', href: '/faq' },
-    { title: 'Data & Sources', href: '/sources' },
-    { title: 'Methodology', href: '/methodology' },
+    {
+        title: 'Home',
+        href: ''
+    },
+    {
+        title: 'FAQs',
+        href: '/faq'
+    },
+    {
+        title: 'Data & Sources',
+        href: '/sources'
+    },
+    {
+        title: 'Methodology',
+        href: '/methodology'
+    }
 ];
