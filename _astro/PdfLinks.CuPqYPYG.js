09faa1dac6e64cee7f81861b85bbb7ad0301312f
narendran-1999 <narendranrita@gmail.com>
2026-03-25 15:04:51 +0530

    Updates

---
+++ _astro/PdfLinks.CuPqYPYG.js
@@ -0,0 +1,34 @@
+import{I as N,a as R,r as U,e as G,c as V,i as J}from"./Icon.Bjq7EiZh.js";import{j as A,k as B,f as I,v as M,w as O,b as m,c as T,d as Q,a as F,i as $,m as S,g as u,s as W,h as a,e as r,r as t,t as j,l as z}from"./snippet.CnAt-I7n.js";import{i as Z}from"./if.DjOCs6rl.js";import{t as L,s as ee,f as te}from"./index.Cpco-vrZ.js";const re="https://drive.google.com/drive/u/0/folders/13cH1bziT64KVwRh7Ak6ewhD36Ta3YpIc",s="https://www.ncrb.gov.in/uploads/",o=["148-149, 153, 174-175, 179","215, 217, 227-228, 267, 269, 279-280","219, 221, 231-232, 271, 273, 283-284","231, 233, 243-244, 285, 287, 297-298"],ae=[{year:2023,pages:o[3],url:s+"files/1CrimeinIndia2023PartI.pdf"},{year:2022,pages:o[3],url:s+"nationalcrimerecordsbureau/custom/1701607577CrimeinIndia2022Book1.pdf"},{year:2021,pages:o[3],url:s+"nationalcrimerecordsbureau/custom/1696831798CII2021Volume1.pdf"},{year:2020,pages:o[2],url:s+"nationalcrimerecordsbureau/post/16959885631653645869CII2020Volume1.pdf"},{year:2019,pages:o[1],url:s+"nationalcrimerecordsbureau/custom/1653730573_CII%202019%20Volume%201.pdf"},{year:2018,pages:o[1],url:s+"nationalcrimerecordsbureau/custom/1653734481_Crime%20in%20India%202018%20-%20Volume%201_3_0_0.pdf"},{year:2017,pages:o[1],url:s+"nationalcrimerecordsbureau/post/16959893381653885627CrimeinIndia2017-Volume100.pdf"},{year:2016,pages:o[0],url:s+"nationalcrimerecordsbureau/custom/1653886924_Crime%20in%20India%20-%202016%20Complete%20PDF%20291117.pdf"}];function se(p,e){A(e,!0);let d=U(e,["$$slots","$$events","$$legacy"]);const l=[["path",{d:"M5 12h14"}],["path",{d:"m12 5 7 7-7 7"}]];N(p,R({name:"arrow-right"},()=>d,{get iconNode(){return l},children:(c,v)=>{var i=B(),n=I(i);M(n,()=>e.children??O),m(c,i)},$$slots:{default:!0}})),T()}function oe(p,e){A(e,!0);let d=U(e,["$$slots","$$events","$$legacy"]);const l=[["path",{d:"M18 6 6 18"}],["path",{d:"m6 6 12 12"}]];N(p,R({name:"x"},()=>d,{get iconNode(){return l},children:(c,v)=>{var i=B(),n=I(i);M(n,()=>e.children??O),m(c,i)},$$slots:{default:!0}})),T()}var ie=$(`<tr class="bg-gray-100"><td class="px-2 py-1 border-2 border-white"> </td><td class="px-2 py-1 border-2 border-white"> </td><td class="px-2 py-1 border-2 border-white"><a target="_blank" class="
+                                    flex px-4 py-2
+                                    font-medium text-nowrap text-center
+                                    bg-red-100 text-red-600
+                                    hover:bg-red-200 hover:text-red-700
+                                    active:bg-red-300 active:text-red-800
+                                    border border-red-200
+                                    transition cursor-pointer
+                                    ">View PDF</a></td></tr>`),ne=$(`<div class="
+            fixed inset-0 z-50
+            flex items-center justify-center
+            bg-black/50
+        "><div class="
+                w-9/10 max-w-lg p-4
+                bg-white shadow-lg
+            "><div class="mb-4 flex items-center justify-between"><h4 class="text-lg font-semibold text-red-600">PDF Links</h4> <button class="
+                        p-2 text-sm
+                        text-red-500 bg-red-100
+                        hover:text-white hover:bg-red-500
+                        active:text-white active:bg-red-500
+                        transition cursor-pointer
+                    "><!></button></div> <a target="_blank" class="
+                flex w-full justify-between items-center
+                px-4 py-2 mb-4
+                font-medium
+                bg-red-100 text-red-600
+                hover:bg-red-200 hover:text-red-700
+                active:bg-red-300 active:text-red-800
+                transition cursor-pointer
+                ">Backup Drive Link (Relevant pages only) <!></a> <table class="w-full"><thead class="bg-gray-200"><tr><th class="px-4 py-2 text-left">Year</th><th class="px-4 py-2 text-left">Pages (in print)</th><th class="px-4 py-2 text-left">Official PDFs</th></tr></thead><tbody></tbody></table></div></div>`),de=$(`<button class="
+    sm:self-start px-6 py-2 mt-2
+    text-white font-medium bg-[#ee2f2f] hover:bg-[#c72727]
+    transition cursor-pointer
+    ">Direct PDF Links to Reports</button> <!>`,1);function me(p){let e=W(!1);function d(){S(e,!u(e))}var l=de(),c=I(l),v=a(c,2);{var i=n=>{var f=ne(),g=r(f),h=r(g),x=a(r(h),2),Y=r(x);oe(Y,{size:20}),t(x),t(h);var b=a(h,2),H=a(r(b));se(H,{size:16}),t(b);var P=a(b,2),C=a(r(P));G(C,21,()=>ae,J,(K,_)=>{var y=ie(),w=r(y),X=r(w,!0);t(w);var k=a(w),q=r(k,!0);t(k);var D=a(k),E=r(D);t(D),t(y),j(()=>{z(X,u(_).year),z(q,u(_).pages),V(E,"href",u(_).url)}),m(K,y)}),t(C),t(P),t(g),t(f),j(()=>V(b,"href",re)),F("click",x,d),L(3,g,()=>ee),L(3,f,()=>te),m(n,f)};Z(v,n=>{u(e)&&n(i)})}F("click",c,d),m(p,l)}Q(["click"]);export{me as default};
---
+++ _astro/ShareButton.B8u_-iC1.js
@@ -0,0 +1,6 @@
+import{I as f,a as l,r as c}from"./Icon.Bjq7EiZh.js";import{c as d,j as p,k as h,f as u,v as g,b as n,w as m,X as b,d as v,a as _,h as y,i as $,e as k,r as w}from"./snippet.CnAt-I7n.js";function x(t,e){p(e,!0);let a=c(e,["$$slots","$$events","$$legacy"]);const o=[["path",{d:"M3.714 3.048a.498.498 0 0 0-.683.627l2.843 7.627a2 2 0 0 1 0 1.396l-2.842 7.627a.498.498 0 0 0 .682.627l18-8.5a.5.5 0 0 0 0-.904z"}],["path",{d:"M6 12h16"}]];f(t,l({name:"send-horizontal"},()=>a,{get iconNode(){return o},children:(s,S)=>{var r=h(),i=u(r);g(i,()=>e.children??m),n(s,r)},$$slots:{default:!0}})),d()}b();var z=$(`<button class="
+        flex gap-3 items-center px-6 py-2
+        border-2 border-gray-100 font-semibold text-gray-100
+        bg-[#ffffff18] hover:bg-[#ffffff37] active:bg-[#ffffff37]
+        transition cursor-pointer
+    ">Share to spread awareness <!></button>`);function N(t){async function e(){window.prompt("Copy this link to share:","https://narendran-1999.github.io/false-cases-india/")}var a=z(),o=y(k(a));x(o,{size:16}),w(a),_("click",a,e),n(t,a)}v(["click"]);export{N as default};
